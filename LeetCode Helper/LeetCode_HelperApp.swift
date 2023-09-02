d2cf46c9d30a612305fd7604ae516a272f6d5075
h200920091 <h200920091@gmail.com>
2023-09-01 23:48:15 -0500

    Initial Commit

---
+++ LeetCode Helper/LeetCode_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCode_HelperApp.swift
+//  LeetCode Helper
+//
+//  Created by 林稚皓 on 2023/9/1.
+//
+
+import SwiftUI
+
+@main
+struct LeetCode_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
